e16c674fcda08aeb41edc2aaf876bce84d14d9ca
rdsande <rd_sande@yahoo.com>
2023-02-03 13:46:11 +0300

    Adding login page

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Smooth scroll on click
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//Toggle Password on Click Lock icon
+// $(".pwd-icon").click(function () {
+
+//     $(".pwd-icon i").toggleClass("ri-lock-fill ri-unlock-fill");
+//     var input = $($(this).attr("toggle"));
+//     if (input.attr("type") == "password") {
+//         input.attr("type", "text");
+//     } else {
+//         input.attr("type", "password");
+//     }
+// });
