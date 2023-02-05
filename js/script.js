3d7dd50c0c0fe4529bec60154b0399438328a6ba
rdsande <rd_sande@yahoo.com>
2023-02-05 10:22:30 +0300

    First part of dashboard design

--- js/script.js
+++ js/script.js
@@ -19,3 +19,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 //         input.attr("type", "password");
 //     }
 // });
+
+// Filter Table on Projects
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("sortProjects");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("projectsTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+$(document).ready(function () {
+    $(".menu ul li").click(function () {
+        $(".menu ul li").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
